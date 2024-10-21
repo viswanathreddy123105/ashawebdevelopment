e10b8fd511a330c62883ccdb25f028e8d5c1b54a
viwanathreddy123105 <viswanathareddyjangallapalle@gmail.com>
2024-10-21 13:57:34 +0530

    first commit

---
+++ arshaa.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.logoarea').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,   
+        autoplaySpeed: 1000, 
+    });
+  });
